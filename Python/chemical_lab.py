a291b5590638c257b4e266d898e670e95b6ff494
Hee jun Jeon <heejun911@naver.com>
2024-08-16 15:57:39 +0900

    Create chemical_lab.py

---
+++ Python/chemical_lab.py
@@ -0,0 +1,16 @@
+```py
+L = input().split()
+min = int(L[0])
+max = int(L[1])
+# min, max = map(int, input().split())
+
+temp = int(input())
+
+while temp != -999:
+    if min <= temp <= max:
+      print('Nothingo to report')
+      temp = int(input())
+    else:
+      print('Alert!')
+      break
+```
